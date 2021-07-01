f0b11cdba852197fb6a631b3984fc06eb4b16574
giodueck <giodueck@gmail.com>
2021-07-01 11:39:31 -0400

    Changed html to require username

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,6 @@ using namespace std;
 
 int main()
 {
-    // uid_t uid = getuid();
-    // // try to switch to root
-    // if (setuid(0))
-    // {
-    //     cerr << "Execute as superuser\n";
-    //     return -1;
-    // }
-    // // recover uid
-    // setuid(uid);
-
     login();
 
     return 0;
