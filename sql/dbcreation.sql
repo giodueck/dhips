6023be2aefc68b004ddaa5645d8ff3910376ad24
giodueck <giodueck@gmail.com>
2021-07-15 20:01:33 -0400

    Added database creation script

---
+++ sql/dbcreation.sql
@@ -0,0 +1,14 @@
+create table login (
+    username varchar(8) not null,
+    pwd varchar(128) not null,
+    constraint login_pk primary key (username)
+);
+
+create table session (
+    session_id serial not null,
+    username varchar(8) not null,
+    expiration timestamp not null,
+    creation timestamp not null,
+    constraint session_pkey primary key (username),
+    constraint username_fkey foreign key (username) references login(username)
+);
