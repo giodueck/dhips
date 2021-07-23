48b18aa4ddbdc53100ac770605b252688e8c24ae
giodueck <giodueck@gmail.com>
2021-07-23 14:53:46 -0400

    Module iii: Added some default targeted sniffers

--- sql/m_iii.sql
+++ sql/m_iii.sql
@@ -1,3 +1,7 @@
 insert into targeted_proc (name, type, active) values
     ('tcpdump', 1, true),
-    ('wireshark', 1, true);
+    ('wireshark', 1, true),
+    ('ethereal', 1, true),
+    ('tethereal', 1, true),
+    ('kismet', 1, true),
+    ('etherape', 1, true);
