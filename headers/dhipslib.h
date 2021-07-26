df4552e5e00645cab0599f1316aeb20cd520bdb6
giodueck <giodueck@gmail.com>
2021-07-26 14:00:24 -0400

    Module iii: moved pid related functions to dhipslib

--- headers/dhipslib.h
+++ headers/dhipslib.h
@@ -4,10 +4,18 @@
 #ifndef DHIPS_H
 #define DHIPS_H
 
+#include <stdlib.h>
+
 // Print error message to stderr in the format 'dhips: perror(msg)'
 void dhips_perror(const char *msg);
 
 // Print error message to stderr in the format 'dhips: msg'
 void dhips_perror_no_errno(const char *msg);
+
+// If a process by the name task_name is found, its pid is copied into *pid
+void getPidByName(pid_t *pid, const char *task_name);
+
+// If a process with the given pid is found, its name is copied into task_name
+void getNameByPid(pid_t pid, char *task_name);
 
 #endif // DHIPS_H
--- src/dhipslib.cpp
+++ src/dhipslib.cpp
@@ -1,6 +1,9 @@
 #include "dhipslib.h"
 #include <iostream>
 #include <string>
+#include <cstring>
+#include <dirent.h>
+#include <limits.h>
 
 using namespace std;
 
@@ -14,4 +17,62 @@ void dhips_perror(const char *msg)
 void dhips_perror_no_errno(const char *msg)
 {
     cerr << "dhips: " << string(msg) << endl;
+}
+
+void getPidByName(pid_t *pid, const char *task_name)
+{
+    DIR *dir;
+    struct dirent *ptr;
+    FILE *fp;
+    char filepath[PATH_MAX];
+    char cur_task_name[FILENAME_MAX];
+    char buf[BUFSIZ];
+
+    dir = opendir("/proc"); 
+    if (NULL != dir)
+    {
+        while ((ptr = readdir(dir)) != NULL) //Cycle read every file/folder under /proc
+        {
+            // Skip if it reads "." or "..", and skip if it reads not the folder name
+            if ((strcmp(ptr->d_name, ".") == 0) || (strcmp(ptr->d_name, "..") == 0))
+                continue;
+            if (DT_DIR != ptr->d_type)
+                continue;
+
+            //Generate the path of the file to be read
+            sprintf(filepath, "/proc/%s/status", ptr->d_name);
+            fp = fopen(filepath, "r");
+            if (NULL != fp)
+            {
+                if( fgets(buf, BUFSIZ-1, fp)== NULL ){
+                    fclose(fp);
+                    continue;
+                }
+                sscanf(buf, "%*s %s", cur_task_name);
+
+                //If the file content meets the requirements, print the name of the path (that is, the PID of the process)
+                if (!strcmp(task_name, cur_task_name)){
+                    sscanf(ptr->d_name, "%d", pid);
+                }
+                fclose(fp);
+            }
+        }
+        closedir(dir);
+    }
+}
+
+void getNameByPid(pid_t pid, char *task_name)
+{
+    char proc_pid_path[BUFSIZ];
+    char buf[BUFSIZ];
+ 
+    sprintf(proc_pid_path, "/proc/%d/status", pid);
+    FILE* fp = fopen(proc_pid_path, "r");
+    if(NULL != fp){
+        if( fgets(buf, BUFSIZ-1, fp)== NULL ){
+            fclose(fp);
+        }
+        fclose(fp);
+        sscanf(buf, "%*s %s", task_name);
+    }
 }
