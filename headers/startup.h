64bbfd8a907773159eacf34e507e78420918ff2e
giodueck <giodueck@gmail.com>
2021-07-27 12:04:36 -0400

    Updated/Added some docs

--- headers/startup.h
+++ headers/startup.h
@@ -1,9 +1,6 @@
 #ifndef STARTUP_H
 #define STARTUP_H
 
-// TODO
-struct config;
-
 // Runs a scan of config files and the database. Applies the starting configurations
 //  Returns 0 if successful, negatives if not
 int startup();
